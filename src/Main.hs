ecd8139868d2ce72175428f50d3b313b26b4c804
Michael Baker <michaeltbaker@gmail.com>
2015-03-07 20:29:25 -0600

    Adds confidence levels to facts

--- src/Main.hs
+++ src/Main.hs
@@ -6,14 +6,24 @@ import System.FilePath.Posix (joinPath)
 import System.Directory      (getHomeDirectory)
 import System.IO.Strict      (readFile)
 
-data Command  = Add    { addQuestion :: String, addAnswer :: String }
-              | Remove { removeFactId :: Integer }
-              | List
-              deriving (Show)
-data Database = Database { dbNextId :: Integer, dbFacts :: [Fact] } deriving (Read, Show)
-data Fact     = Fact { factId :: Integer, factQuestion :: String, factAnswer :: String } deriving (Read, Show)
+data Command    = Add    { addConfidence :: Int, addQuestion :: String, addAnswer :: String }
+                | Remove { removeFactId :: Int }
+                | List
+                deriving (Show)
+data Database   = Database { dbNextId :: Int, dbFacts :: [Fact] } deriving (Read, Show)
+data Fact       = Fact { factConfidence :: Confidence, factId :: Int, factQuestion :: String, factAnswer :: String } deriving (Read, Show)
+data Confidence = Unknown
+                | LittleKnown
+                | Known
+                | WellKnown
+                | Unforgettable
+                deriving (Read, Show, Ord, Eq, Enum, Bounded)
 
-addOptions    = Add <$> argument str (metavar "QUESTION") <*> argument str (metavar "ANSWER")
+confidenceMappings :: [(Int, Confidence)]
+confidenceMappings    = zip [0..] [(minBound :: Confidence)..]
+integerToConfidence i = case drop i [(minBound :: Confidence)..] of [] -> Nothing; (a:_) -> Just a
+
+addOptions    = Add <$> argument auto (metavar "CONFIDENCE") <*> argument str (metavar "QUESTION") <*> argument str (metavar "ANSWER")
 removeOptions = Remove <$> argument auto (metavar "FACTID")
 listOptions   = pure List
 
@@ -33,9 +43,15 @@ run :: Database -> Command -> IO Database
 run db List = do
   mapM_ print (dbFacts db)
   return db
-run db (Add q a) = do
-  let newFact = Fact (dbNextId db) q a
-  return db { dbNextId = dbNextId db + 1, dbFacts = newFact : dbFacts db }
+run db (Add c q a) = do
+  case integerToConfidence c of
+    Just c' -> do
+      let newFact = Fact c' (dbNextId db) q a
+      return db { dbNextId = dbNextId db + 1, dbFacts = newFact : dbFacts db }
+    Nothing -> do
+      putStrLn (show c ++ " is not a valid confidence level.")
+      mapM_ (\(i, x) -> putStrLn $ show i ++ " | " ++ show x) confidenceMappings
+      return db
 run db (Remove i) = do
   let newFacts = filter ((/= i) . factId) $ dbFacts db
   return db { dbFacts = newFacts }
