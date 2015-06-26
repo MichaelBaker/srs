b1b5089bd61fbfec4af55876f7a9c5fd2f8a3960
Michael Baker <michaeltbaker@gmail.com>
2015-06-25 20:10:00 -0700

    Adds view and add commands for lists

--- src/List.hs
+++ src/List.hs
@@ -1,16 +1,34 @@
 module List where
 
 import Control.Applicative ((<$>), (<*>))
+import Data.Monoid         ((<>))
 import Options.Applicative (subparser, command, info, progDesc, argument, metavar, str, helper)
-import ListDatabase        (ListDatabase)
+import Control.Monad       (forM_)
+import ListDatabase        (ListDatabase, List(..), lists, listItems, listName)
 
-data ListCommand = View { listName :: String } deriving (Show)
+data ListCommand = View { listNameToShow :: String }
+                 | Add  { listNameToAdd :: String, listItemToAdd :: String }
+                 deriving (Show)
 
 viewOptions = View <$> argument str (metavar "LIST_NAME")
+addOptions  = Add  <$> argument str (metavar "LIST_NAME") <*> argument str (metavar "ITEM")
 
 options = subparser
-  ( command "view" (info (helper <*> viewOptions) $ progDesc "View the contents of a list")
+  (  command "view" (info (helper <*> viewOptions) $ progDesc "View the contents of a list")
+  <> command "add"  (info (helper <*> addOptions)  $ progDesc "Add an item to a list")
   )
 
 run :: ListDatabase -> ListCommand -> IO ListDatabase
-run db (View _) = return db
+run db (View name) = do
+  forM_ (lists db) $ \list -> do
+    if (listName list) == name
+       then print (listItems list)
+       else return ()
+  return db
+run db (Add name item) = do
+  let listExists = any (\list -> listName list == name) (lists db)
+  return $ if listExists
+     then db { lists = map (addItem name item) (lists db) }
+     else db { lists = List { listName = name, listItems = [item] } : (lists db) }
+
+addItem name item list = if listName list == name then list { listItems = (listItems list) ++ [item] } else list
