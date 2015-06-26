f51d8ec2e5d803afb452454489569b9cf5e81d1e
Michael Baker <michaeltbaker@gmail.com>
2015-06-26 13:26:33 -0700

    Adds the ability to complete a list item

--- src/List.hs
+++ src/List.hs
@@ -2,23 +2,26 @@ module List where
 
 import Control.Applicative ((<$>), (<*>))
 import Data.Monoid         ((<>))
-import Options.Applicative (subparser, command, info, progDesc, argument, metavar, str, helper)
+import Options.Applicative (subparser, auto, command, info, progDesc, argument, metavar, str, helper)
 import Control.Monad       (forM_)
 import ListDatabase        (ListDatabase, List(..), lists, listItems, listName)
 
 data ListCommand = View       { listNameToShow :: String }
                  | Add        { listNameToAdd :: String, listItemToAdd :: String }
                  | CreateList { listNameToAdd :: String }
+                 | Complete   { listNameToComplete :: String, index :: Integer }
                  deriving (Show)
 
-viewOptions   = View       <$> argument str (metavar "LIST_NAME")
-addOptions    = Add        <$> argument str (metavar "LIST_NAME") <*> argument str (metavar "ITEM")
-createOptions = CreateList <$> argument str (metavar "LIST_NAME")
+viewOptions     = View       <$> argument str (metavar "LIST_NAME")
+addOptions      = Add        <$> argument str (metavar "LIST_NAME") <*> argument str (metavar "ITEM")
+createOptions   = CreateList <$> argument str (metavar "LIST_NAME")
+completeOptions = Complete   <$> argument str (metavar "LIST_NAME") <*> argument auto (metavar "ITEM_NUMBER")
 
 options = subparser
-  (  command "view"   (info (helper <*> viewOptions)   $ progDesc "View the contents of a list")
-  <> command "add"    (info (helper <*> addOptions)    $ progDesc "Add an item to a list")
-  <> command "create" (info (helper <*> createOptions) $ progDesc "Create a list")
+  (  command "view"     (info (helper <*> viewOptions)     $ progDesc "View the contents of a list")
+  <> command "add"      (info (helper <*> addOptions)      $ progDesc "Add an item to a list")
+  <> command "create"   (info (helper <*> createOptions)   $ progDesc "Create a list")
+  <> command "complete" (info (helper <*> completeOptions) $ progDesc "Complete an item")
   )
 
 run :: ListDatabase -> ListCommand -> IO ListDatabase
@@ -38,6 +41,11 @@ run db (CreateList name) = do
   if listExists
     then return db
     else return db { lists = List name [] : (lists db) }
+run db (Complete name i) = do
+  let listExists = any (\list -> listName list == name) (lists db)
+  if listExists
+    then return $ completeItem db name i
+    else putStrLn ("The list '" ++ name ++ "' does not exist.") >> return db
 
 addItem name item list = if listName list == name then list { listItems = (listItems list) ++ [item] } else list
 
@@ -45,3 +53,7 @@ printListItems items = mapM_ putStrLn outputLines
   where outputLines = map (pad padding) $ zip ([0..] :: [Integer]) items
         padding     = length $ show $ length items
         pad amount (i, text) = take (amount - (length $ show i)) (repeat ' ') ++ show i ++ "  " ++ text
+
+completeItem db name i = db { lists = map (remove name i) (lists db) }
+  where remove n j list = if listName list == n then list { listItems = itemsWithoutIndex (listItems list) (fromIntegral j) } else list
+        itemsWithoutIndex items idx = take idx items ++ drop (idx + 1) items
